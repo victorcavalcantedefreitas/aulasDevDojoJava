e09173bb0b6d14ef4f2f3261c3bcb6fb1aaea57e
Victor Cavalcante de Freitas <159203722+victorcavalcantedefreitas@users.noreply.github.com>
2025-01-16 20:01:24 -0300

    starting on java!

---
+++ src/academy/devdojo/maratonajava/introducao/aula01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ *  Isso é um comentario <b> java doc</b>
+ */
+
+public class aula01 {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/aula02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class aula02 {
+    public static void main(String[] args) {
+        int ageOfDad = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float SalarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 65;
+        String nome = "Victor";
+        System.out.println("a idade é "+ageOfDad+" anos");
+        System.out.println(verdadeiro);
+        System.out.println(caractere);
+        System.out.println("Opa, meu nome é "+nome);
+    }
+}
