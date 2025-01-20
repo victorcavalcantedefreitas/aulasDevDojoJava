abd90d7afd34703fd76352bed6532c1320b9acbd
Victor Cavalcante de Freitas <159203722+victorcavalcantedefreitas@users.noreply.github.com>
2025-01-20 11:33:32 -0300

    starting on java!

---
+++ src/academy/devdojo/maratonajava/introducao/ex001.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class ex001 {
+    public static void main(String[] args) {
+        String nome = "Victor";
+        String end = "Rua Paulo Barbosa, 67";
+        float salario = 2550.50F;
+        int dia = 17;
+        int mes = 05;
+        int ano = 2009;
+        String msg = "Eu, "+nome+", vivo no endereco "+end+", confirmo que recebi o salario de "+salario+", na data "+dia+"/"+mes+"/"+ano;
+        System.out.println(msg);
+    }
+}
