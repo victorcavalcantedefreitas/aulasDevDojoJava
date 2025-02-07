30d623351ab72756a63a9d1d7fff78c4c1dac0a5
Victor Cavalcante de Freitas <159203722+victorcavalcantedefreitas@users.noreply.github.com>
2025-02-07 20:27:32 -0300

    ajuste no exercicio do DevDojo

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominios/ImpressoraFuncionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominios/ImpressoraFuncionario.java
@@ -1,12 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominios;
 
-import java.util.Arrays;
-
 public class ImpressoraFuncionario {
     public void imprime(Funcionario funcionario){
         System.out.println("os dados são: ");
         System.out.println(funcionario.idade);
         System.out.println(funcionario.nome);
-        System.out.println(Arrays.toString(funcionario.salario));
+        for (int i = 0; i < funcionario.salario.length; i++) {
+            System.out.println("Salario "+(i+1)+": "+ funcionario.salario[i]);
+        }
     }
 }
+
